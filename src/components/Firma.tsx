f28224285679ef644e328c6abed2e0315bdbd1a9
David Garcia Ballester <davidgballester@uoc.edu>
2024-03-05 16:54:28 +0100

    reset signature

--- src/components/Firma.tsx
+++ src/components/Firma.tsx
@@ -2,13 +2,24 @@ import SignatureCanvas from "react-signature-canvas";
 import styles from "./css/Firma.module.scss";
 
 const Firma = () => {
+  let sigCanvas: any;
+  const clear = () => {
+    sigCanvas.clear();
+  };
+
   return (
     <div className="canvasContainer">
       <SignatureCanvas
-        penColor="blue"
+        penColor="#104777"
         clearOnResize={false}
         canvasProps={{ className: styles.sigCanvas }}
+        ref={(ref) => {
+          sigCanvas = ref;
+        }}
       />
+      <button type="button" className={styles.button} onClick={clear}>
+        Reset
+      </button>
     </div>
   );
 };
